b544ea096b2a0ca7bbc0a5072cd1485700cbe47a
1018715564 <SHEN_HAO@users.noreply.github.com>
2018-09-20 14:20:53 +0800

    project init

---
+++ src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+       hello world
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg:'home'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
